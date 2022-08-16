e90532f0f29c6862b576776400fe41d2ef3f7bc1
Aliaksandr <djvalentinoff@gmail.com>
2022-08-16 14:49:20 +0300

    fix: bugs adaptive

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,12 +1,11 @@
 ymaps.ready(['Panel']).then(function () {
   var map = new ymaps.Map("map", {
-    center: [55.762809, 37.624040],
+    center: [55.762090, 37.633095],
     zoom: 14,
     controls: []
   });
   // Создадим контент для меток.
-  var office = '<h3 style="margin-bottom: 5px; margin-left: 10px; font-family: TT Firs Neue; font-style: normal; font-weight: 400; font-size: 24px; line-height: 22px; color: var(--white-color);">Студия «High pass»</h3>' +
-    '<p style="max-width: 301px; margin-bottom: 3px; margin-left: 10px; font-family: Roboto; font-style: normal; font-weight: 400; font-size: 16px; line-height: 28px; color: var(--white-color);">107045, Москва, Даев переулок, дом 41, бизнес-центр «Даев Плаза», этаж 8, офис № 82 <a href="tel:+749542423532" style="margin-left: 2px; font-family: TT Firs Neue; font-style: normal; font-weight: 400; font-size: 16px; line-height: 21px; display: flex; align-items: center; color: var(--burnt-orange-color);"><img src="img/telMap.svg" alt="Телефонная трубка">+7 495 424-23-532</a></p>';
+  var office = '<h3 class="map__title">Студия «High pass»</h3> + <p class="map__text">107045, Москва, Даев переулок, дом 41, бизнес-центр «Даев Плаза», этаж 8, офис № 82</p> + <a class="map__link" href="tel:+749542423532"><img src="img/telMap.svg" alt="Телефонная трубка">+7 495 424-23-532</a>';
   // Создадим и добавим панель на карту.
   var panel = new ymaps.Panel();
   map.controls.add(panel, {
